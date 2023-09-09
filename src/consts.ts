69492817c40e9197d5ff76d3b7b79bdc95ae6350
Dan Martens <dan@friendsoftheweb.com>
2023-09-09 16:09:40 -0400

    Add title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "The Humanities Project";
+export const SITE_DESCRIPTION =
+  "The Humanities Project is a mission-driven organization dedicated to promoting career diversity among PhD-holders in the humanities. We provide resources, inspiration, and advising to humanities PhDs looking to transition into non-faculty roles, whether by choice or due to circumstances.";
