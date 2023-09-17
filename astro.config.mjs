6da762dbed8b61770a04967cc31b86e5bf8d903a
rachelfwaxman <144639407+rachelfwaxman@users.noreply.github.com>
2023-09-17 13:40:27 -0400

    Remove subdirectory from links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://danmartens.com",
-  base: "/humanities-project",
+  site: 'https://example.com',
   integrations: [mdx(), sitemap()],
 });
