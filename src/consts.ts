17eb64752be43505d81f385c7e1148e4da3e3d7e
rachelfwaxman <144639407+rachelfwaxman@users.noreply.github.com>
2024-02-19 19:47:26 -0500

    Create SIte Tag

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "The Humanities Project";
+export const SITE_TAG = "Furthering Career Diversity for Humanities PhDs";
 export const SITE_DESCRIPTION =
   "The Humanities Project is a mission-driven organization dedicated to promoting career diversity among PhD-holders in the humanities. We provide resources, inspiration, and advising to humanities PhDs looking to transition into non-faculty roles, whether by choice or due to the limited academic job market.";
